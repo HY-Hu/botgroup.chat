f61e3e8fb2100819133c1ff3837443ca4faab219
HY-Hu <138071947+HY-Hu@users.noreply.github.com>
2025-06-17 12:33:15 +0800

    Update groups.ts

--- src/config/groups.ts
+++ src/config/groups.ts
@@ -11,7 +11,7 @@ export const groups: Group[] = [
     id: 'group1',
     name: '🔥A1交流群',
     description: '群消息关注度权重："user"的最新消息>其他成员最新消息>"user"的历史消息>其他成员历史消息',
-    members: ['ai8', 'ai7', 'ai9', 'ai10', 'ai4', 'ai11'],
+    members: ['ai8', 'ai7', 'ai10', 'ai11'],
     isGroupDiscussionMode: false
   }
 ];
